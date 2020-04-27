04a8939e5832bdbcb3f2616dd4e586e0c8c60467
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-04-27 13:24:45 +0100

    added login code (not functioning)

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var handlebars = require('express-handlebars');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var session = require('express-session')
+require('./middleware/passport')(passport);
+
+var { isAuth } = require('./middleware/isAuth')
 
 var User = require('./models/User.js');
 
@@ -14,6 +19,14 @@ app.engine('hbs', handlebars({
 }))
 
 app.use(express.static('public'));
+app.use(session({
+    secret: 'mySecret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,6 +34,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get ('/', (req, res) =>{
     res.render('login', { layout: 'main' });
 })
+
+app.get ('/cogfeed', isAuth, (req, res) => {
+    res.render('cogfeed' , {layout: 'mainpage', username: req.user.username});
+});
 
 app.post('/signup', async (req, res) =>{
     const { email, username, password } = req.body;
@@ -46,6 +63,18 @@ app.post('/signup', async (req, res) =>{
         console.log(err.message);
         res.status(500).send('Server Error')
     }  
+})
+
+app.post('/signin', (req, res, next) => {
+    try{
+        passport.authenticate('local', {
+            successRedirect: '/cogfeed',
+            failureRedirect: '/'
+        })(req, res, next);
+    } catch (err){
+        console.log(err.message);
+        res.status(500).send('Server Error')
+    }
 })
 
 mongoose.connect('mongodb://localhost:27017/handlebars',{
