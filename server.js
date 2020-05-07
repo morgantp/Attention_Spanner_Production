5a4170c75e8d4cb88a2e8ad76f77213f9432c1dd
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-05-07 10:14:20 +0100

    heroku fix 2

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ mongoose.connect(mongoURL, {
     console.log('Not Connected to the DB with err : ' +err);
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
